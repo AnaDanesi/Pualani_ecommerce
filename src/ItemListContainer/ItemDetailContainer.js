ea635be22aaddc1b2e769166d85fc65c482bcdce
Anabella Danesi <anetadanesi@gmail.com>
2022-01-03 21:15:59 -0300

    arreglando el array de producto

--- src/ItemListContainer/ItemDetailContainer.js
+++ src/ItemListContainer/ItemDetailContainer.js
@@ -4,12 +4,12 @@ import ItemDetail from "./ItemDetail";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const listaProductos = [
-    {id:1,nombre: "Maceta Flora" , foto: <img src="/macetaFlora.png" alt=""/>},
-    {id:2,nombre: "Combo Paila" , foto: <img src="/comboPaila.png" alt=""/>} ,
-    {id:3,nombre: "Maceta Canelon" , foto: <img src="/macetaCanelon.png" alt=""/>},
-    {id:4,nombre: "Combo Bandeja Oval", foto: <img src="/bandejaOval.png" alt=""/>},
-    {id:5,nombre: "Combo Maceta Rectangular", foto:<img src="/bandejaRectangular.png" alt=""/>} ,
-    {id:6,nombre: "Maceta Geo" , foto: <img src="/macetaGeo.png" alt=""/>},
+    {id:1,nombre: "Maceta Flora" , foto: "/macetaFlora.png", precio: 650, stock:2, info: "color ladrillo o amarillo" } ,
+    {id:2,nombre: "Combo Paila" , foto: "/comboPaila.png", precio: 1500,  stock:5, info: "color gris con franja a eleccion"} ,
+    {id:3,nombre: "Maceta Canelon" , foto: "/macetaCanelon.png", precio: 750, stock:5, info: "color ladrillo, amarillo, verde"} ,
+    {id:4,nombre: "Combo Bandeja Oval", foto: "/bandejaOval.png",precio: 1250, stock:5, info: "color blanco,gris o marmolado" } ,
+    {id:5,nombre: "Combo Maceta Rectangular", foto:"/bandejaRectangular.png", precio: 1250, stock:3, info: "color blanco, verde, amarillo" } ,
+    {id:6,nombre: "Maceta Geo" , foto: "/macetaGeo.png", precio: 900, stock:1, info: "color ladrillo,gris o amarillo" } ,
 ]
 
 function ItemDetailContainer() {
